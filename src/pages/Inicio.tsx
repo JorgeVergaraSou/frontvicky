42454889e3046c0c931053aa939978c50d643832
jorge vergara <jorgevergarasou@gmail.com>
2024-05-20 14:50:19 -0300

    mascotas page

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,49 +1,11 @@
-import { useEffect, useState } from 'react'
+//import { useEffect, useState } from 'react'
 import imgUrl from '../assets/logoRounded.png';
 
 export const Inicio: React.FC = () => {
-  const [posts, setPosts] = useState<Post[] | null>(null);
-  //const [posts, setPosts] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
+
   
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const res = await fetch(`http://localhost:3006/api/v1/posts`, {
-       // const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/posts`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        if (!res.ok) {
-          throw new Error('Error al obtener los datos');
-        }
-        const data = await res.json();
-        setPosts(data);
-      } catch (err) {
-        if (err instanceof Error) {
-          setError(err.message);
-        } else {
-          setError('An unknown error occurred');
-        }
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchPosts();
-  }, []);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>Error: {error}</p>;
-  }
+ 
 
   return (
     <>
@@ -114,26 +76,6 @@ export const Inicio: React.FC = () => {
         </p>
       </div>
 
-      <div className="divCardPet">
-        {posts && posts.map(post => (
-          <div className="cardDiv" key={post.idPost}>
-            <h3>{post.title}</h3>
-            <p>{post.content}</p>
-            {post.pets.length > 0 && (
-              <div key={post.pets[0].idPet}>
-                <p>Nombre de la mascota: {post.pets[0].namePet}</p>
-                <p>Descripción: {post.pets[0].description}</p>
-                <img
-                  src={`/img/${post.pets[0].image}`}
-                  width={150}
-                  height={150}
-                  alt="Imagen de la mascota"
-                />
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
     </>
   );
 };
--- src/pages/Mascotas.tsx
+++ src/pages/Mascotas.tsx
@@ -1,59 +1,80 @@
-import React from 'react'
-import { Card } from '../components/components'
+import React, { useEffect, useState } from 'react'
+//import { Card } from '../components/components'
 
-interface Pet {
-  id: number
-  name: string
-  dateLost: string
-  description: string
-  imageUrl: string
-  tags: [breed: string, age: string]
-}
 
-const mascotasData: Pet[] = [
-  {
-    id: 1,
-    name: 'Luna',
-    dateLost: '',
-    description: '',
-    imageUrl: 'https://example.com/luna.jpg',
-    tags: ['Golden Retriever', '3 años']
-  },
-  {
-    id: 2,
-    name: 'Max',
-    dateLost: '',
-    description: '',
-    imageUrl: 'https://example.com/max.jpg',
-    tags: ['German Shepherd', '5 años']
-  }
-  // Agrega más mascotas si lo deseas
-]
 
 export const Mascotas: React.FC = () => {
+
+const [posts, setPosts] = useState<Post[] | null>(null);
+//const [posts, setPosts] = useState(null);
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState<string | null>(null);
+
+
+useEffect(() => {
+  const fetchPosts = async () => {
+    try {
+      const responsePost = await fetch(`http://localhost:3006/api/v1/posts`, {
+     // const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/posts`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!responsePost.ok) {
+        throw new Error('Error al obtener los datos');
+      }
+      const dataPost = await responsePost.json();
+      setPosts(dataPost);
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError('An unknown error occurred');
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  fetchPosts();
+}, []);
+
+if (loading) {
+  return <p>Loading...</p>;
+}
+
+if (error) {
+  return <p>Error: {error}</p>;
+}
+
   return (
+    <>
     <div>
       <h1 className='text-2xl font-bold mb-4'>Mascotas</h1>
-        <div className='mascotas-container flex flex-wrap gap-4 p-justify-center'>
-          {mascotasData.map(pet => (
-            <Card
-              imageUrl={pet.imageUrl}
-              petName={pet.name}
-              dateLost={pet.dateLost}
-              petDescription={pet.description}
-              petTags={pet.tags}
-            />
-            // <div
-            //   key={pet.id}
-            //   className='mascota-card w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5'
-            // >
-            //   <img src={pet.imageUrl} alt={pet.name} />
-            //   <h2>{pet.name}</h2>
-            //   <p>Raza: {pet.breed}</p>
-            //   <p>Edad: {pet.age} años</p>
-            // </div>
-          ))}
-        </div>
+        
+        <div className="divCardPet">
+        {posts && posts.map(post => (
+          <div className="cardDiv" key={post.idPost}>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+            {post.pets.length > 0 && (
+              <div key={post.pets[0].idPet}>
+                <p>Nombre de la mascota: {post.pets[0].namePet}</p>
+                <p>Descripción: {post.pets[0].description}</p>
+                <img
+                  src={`/img/${post.pets[0].image}`}
+                  width={150}
+                  height={150}
+                  alt="Imagen de la mascota"
+                />
+              </div>
+            )}
+          </div>
+        ))}
       </div>
+       
+      </div>
+      </>
   )
 }
